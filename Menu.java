f6eb5fd3dca919bff7099bfe2373163845d7e970
abhinav6999 <89853717+abhinav6999@users.noreply.github.com>
2021-09-04 17:45:26 +0530

    project

---
+++ Menu.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Menu{
+    public static void main(String[] args){
+        System.out.println("Enter the number through menu list:");
+        Scanner input=new Scanner(System.in);
+        int numeric=input.nextInt();
+        if(numeric==1){
+            notepad();
+        }
+        else if(numeric==2){
+            calc();
+        }
+        else if(numeric==3){
+            chrome();
+        }
+    }
+    static void notepad(){
+        Runtime app = Runtime.getRuntime();
+        try{
+            app.exec("notepad");
+           
+        }
+        catch (Exception Ex){
+            System.out.println("Error: " + Ex.toString());
+        }
+    }
+    static void calc(){
+        Runtime app = Runtime.getRuntime();
+        try{
+            app.exec("calc");
+           
+        }
+        catch (Exception Ex){
+            System.out.println("Error: " + Ex.toString());
+        }
+    }
+    static void chrome(){
+        Runtime app = Runtime.getRuntime();
+        try{
+            app.exec("chrome");
+           
+        }
+        catch (Exception Ex){
+            System.out.println("Error: " + Ex.toString());
+        }
+    }
+
+}
